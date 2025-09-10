0b9f46941a37c6d9758b98d9810dec5127bd11d9
AsadullahSamo <asadsamoon8@gmail.com>
2025-09-10 23:10:13 +0500

    Remove unused user management scripts and SQL files

--- delete_users.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo WARNING: This will delete users without data!
-echo.
-cd django-backend\backend
-python manage.py delete_users_without_data
-pause 
--- run_user_cleanup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Running user cleanup script...
-cd django-backend\backend
-python manage.py delete_users_without_data --dry-run
-pause 
