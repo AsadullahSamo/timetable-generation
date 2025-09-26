30b0ae6f6300ba07e3542aa4c363a5a4bb37e021
AsadullahSamo <asadsamoon8@gmail.com>
2025-09-26 22:51:43 +0500

    feat: Register TeacherSubjectAssignment in admin and clean up unregistered models

--- django-backend/backend/timetable/admin.py
+++ django-backend/backend/timetable/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Config, ClassGroup, Subject, Teacher, Classroom, ScheduleConfig, TimetableEntry
+from .models import Subject, Teacher, Classroom, ScheduleConfig, TimetableEntry, TeacherSubjectAssignment
 
-admin.site.register(Config)
-admin.site.register(ClassGroup)
+# Note: Group and Celery models are unregistered in apps.py ready() method
+
+# Register models (removed Config and ClassGroup as requested)
 admin.site.register(Subject)
 admin.site.register(Teacher)
 admin.site.register(Classroom)
 admin.site.register(ScheduleConfig)
 admin.site.register(TimetableEntry)
+
+# Add TeacherSubjectAssignment that was missing from admin
+admin.site.register(TeacherSubjectAssignment)
